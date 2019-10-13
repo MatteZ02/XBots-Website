6972bcdfbe809068ce8eba88af5a776dfbfb005f
AseCoder <atte.elo05@gmail.com>
2019-10-13 15:44:18 +0300

    launch

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+const Discord = require('discord.js');
+const client = new Discord.Client(); // Client = FutoX
+const app = express();
+const port = process.env.PORT || 5000;
+const coredevs = []; 
+let avatars = {};
+require('dotenv/config');
+
+client.login(process.env.FUTOXTOKEN);
+
+client.on('ready', async () => {
+  client.user.setStatus('idle');
+  console.log('- Futox Ready -');
+  await client.users.fetch('607266889537945605');
+
+  const devs = client.guilds.get('583597555095437312' /* xbots support */).roles.get('632915758422163476' /* futox devs */ ).members;
+  devs.forEach(guildMember => {
+    coredevs.push({
+      tag: guildMember.user.tag,
+      id: guildMember.user.id,
+      avatarURL: guildMember.user.displayAvatarURL({ format: 'png', size: 512, }),
+    });
+  });
+  avatars = {
+    futox: client.user.displayAvatarURL({ format: 'png', size: 2048, }),
+    musix: client.users.get('607266889537945605').displayAvatarURL({ format: 'png', size: 2048, }),
+  };
+});
+
+app.listen(port, () => console.log(`- Active on port ${port} -`));
+app.get('/api', (rep, res) => { 
+  res.json({ coredevs, avatars, });
+});
+
+app.get('/', express.static('public/home'));
+app.get('/activity', express.static('public/activity'));
