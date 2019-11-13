678bb3e1a35c5d2770583f09f606f0390f263412
AseCoder <atte.elo05@gmail.com>
2019-11-13 22:07:56 +0200

    its responsive now

--- public/home/app.js
+++ public/home/app.js
@@ -1,8 +1,8 @@
-/*const Http = new XMLHttpRequest();
+const Http = new XMLHttpRequest();
 const url = '/api';
 Http.open("GET", url);
 Http.send();
 Http.onreadystatechange = (e) => {
   if (e.target.readyState === 4) {
   }
-}*/
+}
