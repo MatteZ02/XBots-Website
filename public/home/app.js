16c2df8e559606a9c821ab24310c4e25950f2a1e
AseCoder <atte.elo05@gmail.com>
2019-11-13 17:36:10 +0200

    Update app.js

--- public/home/app.js
+++ public/home/app.js
@@ -1,20 +1,8 @@
-const Http = new XMLHttpRequest();
+/*const Http = new XMLHttpRequest();
 const url = '/api';
 Http.open("GET", url);
 Http.send();
 Http.onreadystatechange = (e) => {
   if (e.target.readyState === 4) {
-    const response = JSON.parse(e.target.responseText);
-    console.log(response);
-    $('.avatar.futox').css('background-image', `url(${response.avatars.futox})`);
-    $('.avatar.musix').css('background-image', `url(${response.avatars.musix})`);
-    $('.name.futox').css({
-      'color': response.colors.futox,
-      'text-shadow': `0 0 15px ${response.colors.futox}`,
-    });  
-    $('.name.musix').css({
-      'color': response.colors.musix,
-      'text-shadow': `0 0 15px ${response.colors.musix}`,
-    });
   }
-}
+}*/
